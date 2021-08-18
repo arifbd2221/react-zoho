6652a15b8ef0ed61572662679bb46c661f9897ab
arifbd2221 <mohidulhoque216@gmail.com>
2021-08-18 18:05:54 +0600

    added flask server side codes

---
+++ api.py
@@ -0,0 +1,69 @@
+import flask
+import requests
+from flask import request, jsonify
+from flask_cors import CORS, cross_origin
+import json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+def get_access_token():
+    url = 'https://accounts.zoho.com/oauth/v2/token'
+
+    params = {
+        'grant_type': 'refresh_token',
+        'client_id': '1000.JJUC3ODSMPUMKXP4APEWJN9NT9LTZB',
+        'client_secret': 'bc8a43aaccc033ae95860d0b2db9a46ee8777eb2cb',
+        'refresh_token': '1000.5121016c92d607f08eebc8b8e8091ee6.adf13963fc1959c05fd2c86a819371e1'
+    }
+    
+    response = requests.post(url=url, params=params)
+    
+    return response.json().get("access_token")
+
+
+@app.route('/users', methods=['GET'])
+@cross_origin()
+def home():
+    token = get_access_token()
+    url = 'https://www.zohoapis.com/crm/v2/Leads'
+
+    headers = {
+        'Authorization': f'Zoho-oauthtoken {token}',
+        'If-Modified-Since': '2020-03-19T17:59:50+05:30'
+    }
+
+    response = requests.get(url=url, headers=headers, params=None)
+
+    if response is not None:
+        print("HTTP Status Code : " + str(response.status_code))
+        return response.json()
+    
+    return "Nothing"
+
+
+@app.route("/add", methods=["POST"])
+@cross_origin()
+def add_user():
+    token = get_access_token()
+    
+    form_data = dict()
+    form_data["data"] = [request.json]
+    
+    url = 'https://www.zohoapis.com/crm/v2/Leads'
+
+    headers = {
+        'Authorization': f'Zoho-oauthtoken {token}',
+    }
+    
+    response = requests.post(url=url, headers=headers, data=json.dumps(form_data).encode('utf-8'))
+    if response.ok:
+        print("OK", response)
+    print(response)
+    return "Nothing"
+
+app.run()
